44f889d2b4c44f5065dd49fec252faafff815562
Andy <andypark@theaiinstitute.com>
2023-09-22 05:56:41 -0400

    a few updates
    - dark mode
    - slider bar size
    - added tabs with joint slider bars

--- example_nicegui.py
+++ example_nicegui.py
@@ -1,42 +1,8 @@
-# from nicegui import ui
-# from nicegui.events import ValueChangeEventArguments
-
-
-# class Demo:
-#     def __init__(self):
-#         self.number = 1
-
-
-# def show(event: ValueChangeEventArguments):
-#     name = type(event.sender).__name__
-#     ui.notify(f"{name}: {event.value}")
-
-
-# ui.button("Button", on_click=lambda: ui.notify("Click"))
-# with ui.row():
-#     ui.checkbox("Checkbox", on_change=show)
-#     ui.switch("Switch", on_change=show)
-# ui.radio(["A", "B", "C"], value="A", on_change=show).props("inline")
-
-# # with ui.row():
-# #     ui.input("Text input", on_change=show)
-# #     ui.select(["One", "Two"], value="One", on_change=show)
-# # ui.link("And many more...", "/documentation").classes("mt-8")
-
-# demo = Demo()
-# v = ui.checkbox("visible", value=True)
-# with ui.column().bind_visibility_from(v, "value"):
-#     ui.slider(min=1, max=3).bind_value(demo, "number")
-#     ui.toggle({1: "A", 2: "B", 3: "C"}).bind_value(demo, "number")
-#     ui.number().bind_value(demo, "number")
-
-# ui.run()
-
-
 from datetime import datetime
 from nicegui import ui
 from nicegui.events import ValueChangeEventArguments
 
+dark = ui.dark_mode()
 
 class Demo:
     def __init__(self):
@@ -66,29 +32,87 @@ class Demo:
 
 demo = Demo()
 
-ui.label("Teleop Helper Menu")
-with ui.row():
-    ui.button("Position Only", on_click=lambda: ui.notify("Position Only"))
-    ui.button("Rotation Only", on_click=lambda: ui.notify("Rotation Only"))
-    ui.button("Both", on_click=lambda: ui.notify("Both"))
-with ui.row():
-    slider_pos = ui.slider(min=0, max=100, value=50).bind_value(
-        demo, "position_sensitivity"
+with ui.header().classes(replace="row items-center") as header:
+    ui.button(on_click=lambda: left_drawer.toggle(), icon="menu").props(
+        "flat color=white"
     )
-    ui.label().bind_text_from(slider_pos, "value")
-    slider_rot = ui.slider(min=0, max=100, value=50).bind_value(
-        demo, "rotation_sensitivity"
-    )
-    ui.label().bind_text_from(slider_rot, "value")
-with ui.row():
-    ui.button("Start Recording", on_click=demo.start_recording)
-    ui.button("Pause Recording", on_click=demo.pause_recording)
-    ui.button("Stop Recording", on_click=demo.stop_recording)
-ui.label(f"Start Time for Record: {demo.start_time}").bind_visibility_from(
-    demo, "recording"
-)
-with ui.row():
-    switch = ui.switch("switch me")
-    ui.label("Switch!").bind_visibility_from(switch, "value")
+    with ui.tabs() as tabs:
+        ui.tab("Teleop Settings")
+        ui.tab("Robot Status")
+        ui.tab("UI Settings")
+
+with ui.footer(value=False) as footer:
+    ui.label("Footer")
+
+with ui.left_drawer().classes("bg-blue-100") as left_drawer:
+    ui.label("Side menu")
+
+with ui.page_sticky(position="bottom-right", x_offset=20, y_offset=20):
+    ui.button(on_click=footer.toggle, icon="contact_support").props("fab")
+
+with ui.tab_panels(tabs, value="Teleop Settings").classes("w-full"):
+    with ui.tab_panel("Teleop Settings"):
+        ui.label("Selected axes for teleop")
+        with ui.row():
+            ui.button("Position Only", on_click=lambda: ui.notify("Position Only"))
+            ui.button("Rotation Only", on_click=lambda: ui.notify("Rotation Only"))
+            ui.button("Both", on_click=lambda: ui.notify("Both"))
+        # with ui.row():
+
+        ui.label('position sensitivity')
+        slider_pos = ui.slider(min=0, max=100, value=50).bind_value(
+            demo, "position_sensitivity"
+        )
+        ui.label().bind_text_from(slider_pos, "value")
+
+        ui.label('rotation sensitivity')
+        slider_rot = ui.slider(min=0, max=100, value=50).bind_value(
+            demo, "rotation_sensitivity"
+        )
+        ui.label().bind_text_from(slider_rot, "value")
+
+        ui.label('gripper max force [N]')
+        slider_rot = ui.slider(min=0, max=50, value=25)
+        ui.label().bind_text_from(slider_rot, "value")
+
+        with ui.row():
+            ui.button("Start Recording", on_click=demo.start_recording)
+            ui.button("Pause Recording", on_click=demo.pause_recording)
+            ui.button("Stop Recording", on_click=demo.stop_recording)
+        ui.label(f"Start Time for Record: {demo.start_time}").bind_visibility_from(
+            demo, "recording"
+        )
+        with ui.row():
+            switch = ui.switch("switch me")
+            ui.label("Switch!").bind_visibility_from(switch, "value")
+    with ui.tab_panel("Robot Status"):
+        ui.label("Joint Positions")
+        ui.label('A1')
+        slider_pos = ui.slider(min=0, max=1, step=0.1, value=0.5)
+        ui.label().bind_text_from(slider_pos, "value")
+        ui.label('A2')
+        slider_pos = ui.slider(min=0, max=1, step=0.1, value=0.5)
+        ui.label().bind_text_from(slider_pos, "value")
+        ui.label('A3')
+        slider_pos = ui.slider(min=0, max=1, step=0.1, value=0.5)
+        ui.label().bind_text_from(slider_pos, "value")
+        ui.label('A4')
+        slider_pos = ui.slider(min=0, max=1, step=0.1, value=0.5)
+        ui.label().bind_text_from(slider_pos, "value")
+        ui.label('A5')
+        slider_pos = ui.slider(min=0, max=1, step=0.1, value=0.5)
+        ui.label().bind_text_from(slider_pos, "value")
+        ui.label('A6')
+        slider_pos = ui.slider(min=0, max=1, step=0.1, value=0.5)
+        ui.label().bind_text_from(slider_pos, "value")
+        ui.label('A7')
+        slider_pos = ui.slider(min=0, max=1, step=0.1, value=0.5)
+        ui.label().bind_text_from(slider_pos, "value")
+    with ui.tab_panel("UI Settings"):
+        ui.label('Switch mode:')
+        ui.button('Dark', on_click=dark.enable)
+        ui.button('Light', on_click=dark.disable)
+
+
 
 ui.run()
